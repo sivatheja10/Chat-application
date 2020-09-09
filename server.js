0f936a094009fd6bdc809678b35788c291b29967
sivatheja10 <12194495+sivatheja10@users.noreply.github.com>
2020-09-09 12:09:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ io.on('connection',socket =>{
     });
 });
 // Declaring the custom server PORT number
-const PORT = 8000 || process.env.PORT;
+const PORT = 80 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
